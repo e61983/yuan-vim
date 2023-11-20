9de3396f1f3cad02a69ee4bc7d7466dabf037877
Yuan <e61983@gmail.com>
2023-11-20 14:53:53 +0800

    chore: add configuration for highlighting in vim-go.lua
    - Add configuration for highlighting types, fields, functions, function calls, operators, extra types, and build constraints in vim-go.lua

--- nvim/lua/plugins/vim-go.lua
+++ nvim/lua/plugins/vim-go.lua
@@ -2,4 +2,13 @@ return {
   'fatih/vim-go',
   run = ':GoUpdateBinaries',
   ft = 'go',
+  config = function ()
+    vim.g.go_highlight_types = true
+    vim.g.go_highlight_fields = true
+    vim.g.go_highlight_functions = true
+    vim.g.go_highlight_function_calls = true
+    vim.g.go_highlight_operators = true
+    vim.g.go_highlight_extra_types = true
+    vim.g.go_highlight_build_constraints = true
+  end
 }
