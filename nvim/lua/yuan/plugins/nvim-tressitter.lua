91a467f1777c8e9099a15ae051e860297bc755d2
Yuan <e61983@gmail.com>
2026-06-24 13:31:28 +0800

    refactor(treesitter): migrate to main branch
    The master branch is in maintenance mode. Move to the main branch,
    which drops the module system: highlight and folds now run from a
    FileType autocmd and parsers install via install(). Remove the
    incremental_selection keymaps and the indent module, with no
    main-branch equivalent kept.

--- nvim/lua/yuan/plugins/nvim-tressitter.lua
+++ nvim/lua/yuan/plugins/nvim-tressitter.lua
@@ -1,51 +1,40 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
-	event = { "BufReadPre", "BufNewFile" },
+	branch = "main",
+	lazy = false,
 	build = ":TSUpdate",
-	-- dependencies = {
-	--   "windwp/nvim-ts-autotag",
-	-- },
 	config = function()
-		local treesitter = require("nvim-treesitter.configs")
-		treesitter.setup({
-			highlight = {
-				enable = true,
-			},
-			indent = { enable = true },
-			autotag = {
-				enable = true,
-			},
-			ensure_installed = {
-				"json",
-				"javascript",
-				"yaml",
-				"html",
-				"css",
-				"markdown",
-				"markdown_inline",
-				"svelte",
-				"graphql",
-				"bash",
-				"lua",
-				"vim",
-				"dockerfile",
-				"gitignore",
-				"vimdoc",
-				"c",
-				"go",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<bs>",
-				},
-			},
+		require("nvim-treesitter").install({
+			"json",
+			"javascript",
+			"yaml",
+			"html",
+			"css",
+			"markdown",
+			"markdown_inline",
+			"svelte",
+			"graphql",
+			"bash",
+			"lua",
+			"vim",
+			"dockerfile",
+			"gitignore",
+			"vimdoc",
+			"c",
+			"go",
 		})
-		vim.wo.foldmethod = "expr"
-		vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-		vim.wo.foldlevel = 99
+
+		-- main 分支移除了 module 系統，highlight 改由 Neovim 提供。
+		-- 對每個有 parser 的 buffer 啟用 highlight 與 treesitter 折疊。
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function()
+				if not pcall(vim.treesitter.start) then
+					return
+				end
+				vim.wo[0][0].foldmethod = "expr"
+				vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+				vim.wo[0][0].foldlevel = 99
+			end,
+		})
 	end,
 }
